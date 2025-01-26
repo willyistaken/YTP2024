23d6fed33a43940ecf5b253552f31a5392e14133
qwe1rt1yuiop1 <ynnek0130@gmail.com>
2025-01-26 21:08:26 +0800

    tab comparer added

---
+++ tabComparer/formatChecker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR=$1
+ERROR_LOG="error_log.txt"
+
+if [ -z "$DIR" ]; then
+	echo "Usage: $0 <directory>"
+	exit 1
+fi
+
+> "$ERROR_LOG"
+
+find "$DIR" -type f -name "*.txt" | while read -r file; do
+	./a.out "$file" "$file" 2>> "$ERROR_LOG"
+done
