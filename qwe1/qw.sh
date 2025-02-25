8a691c69dd9fc79bba6c608f8ea2e2bcf9359648
qwe1rt1yuiop1 <ynnek0130@gmail.com>
2025-02-25 22:16:38 +0800

    hist DP added

--- qwe1/qw.sh
+++ qwe1/qw.sh
@@ -1,3 +1,3 @@
-python3 ../qwe1/genNote.py $1 > ../qwe1/note.txt
+python3 ../qwe1/genNote.py "$1" > ../qwe1/note.txt
 ../qwe1/a.out ../qwe1/stat.txt ../qwe1/note.txt > ../qwe1/tabArray.txt
 python3 ../qwe1/outputTrack.py < ../qwe1/tabArray.txt
