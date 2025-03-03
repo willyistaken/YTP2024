0fc8c23c82728a294ac6ce20f08fc72cb6917975
willyistaken <willyhlchan@gmail.com>
2025-03-03 15:53:48 +0800

    fix lib

--- scorescript/o1.py
+++ scorescript/o1.py
@@ -1,6 +1,14 @@
 print("2 ",end='');
 with open("temp.txt") as f:
-    a = f.readline();
-    n = len(a)-2;
-    for i in range(0,n-1):
-        print(1,end='');
+    a = f.readlines();
+    n = len(a[0]);
+    tab_array=[];
+    bar=0;
+    note=0;
+    for k in range(0,n):
+        if(a[0][k]=='|'):
+            bar+=1;
+            note=0;
+            continue; 
+            for
+          
--- scorescript/run.sh
+++ scorescript/run.sh
@@ -35,22 +35,22 @@ for mid_file in "$folder"/*.mid; do
 
 	#CHANGE HERE , whatever your program do here, just output it to temp.txt, $1 is your program(which is the argument this script take)
 	#python "$mid_file" > temp.txt
-	#cp "$mid_file" ~/code/YTP/tabsdata/algo/tuttut/midis/a.mid
-    #cd ~/code/YTP/tabsdata/algo/tuttut/
-	#python midi_tabs_cli.py "a.mid" > /dev/null
-	#cp ~/code/YTP/tabsdata/algo/tuttut/tabs/a.txt ~/code/YTP2024/scorescript/temp.txt
-	#cd ~/code/YTP2024/scorescript/.
-	#python o1.py >> temp.txt	
+	cp "$mid_file" ~/code/YTP/tabsdata/algo/tuttut/midis/a.mid
+    cd ~/code/YTP/tabsdata/algo/tuttut/
+	python midi_tabs_cli.py "a.mid" > /dev/null
+	cp ~/code/YTP/tabsdata/algo/tuttut/tabs/a.txt ~/code/YTP2024/scorescript/temp.txt
+	cd ~/code/YTP2024/scorescript/.
+	python o1.py >> temp.txt	
 	
 	# "$1" "$mid_file" > temp.txt
-	python3 "$1" "$mid_file" > temp.txt
+	#python3 "$1" "$mid_file" > temp.txt
 
 	
 	if [[ $? -eq 0 ]]; then
 		echo 1 >> result.txt
 		# ./a.out "$txt_file" temp.txt >> result.txt
 		# ./armb.out "$txt_file" temp.txt >> result.txt
-		./armc.out "$txt_file" temp.txt >> result.txt
+		./a.out "$txt_file" temp.txt >> result.txt
 		
 		if [[ $? -eq 0 ]]; then
 			:
